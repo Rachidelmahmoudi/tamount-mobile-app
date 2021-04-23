d43fdf1afa93fe3c6a05e70323898341e46748a1
Rachidelmahmoudi <rachidelmahmoudi@hotmail.com>
2021-04-23 06:16:42 +0000

    first app commit

---
+++ screens/MainTabScreen.js
@@ -0,0 +1,108 @@
+import React from 'react';
+
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import HomeScreen from './HomeScreen';
+import DetailsScreen from './DetailsScreen';
+import ExploreScreen from './ExploreScreen';
+import ProfileScreen from './ProfileScreen';
+
+const HomeStack = createStackNavigator();
+const DetailsStack = createStackNavigator();
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MainTabScreen = () => (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#fff"
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeStackScreen}
+        options={{
+          tabBarLabel: 'لائحة السكان',
+          tabBarColor: '#f1a54f',
+          tabBarIcon: ({ color }) => (
+            <Icon name="ios-home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={DetailsStackScreen}
+        options={{
+          tabBarLabel: 'العمليات',
+          tabBarColor: '#0068b0',
+          tabBarIcon: ({ color }) => (
+            <Icon name="ios-notifications" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: 'وثائق',
+          tabBarColor: '#ec2539',
+          tabBarIcon: ({ color }) => (
+            <Icon name="ios-person" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Explore"
+        component={ExploreScreen}
+        options={{
+          tabBarLabel: 'اخبار',
+          tabBarColor: '#48c229',
+          tabBarIcon: ({ color }) => (
+            <Icon name="ios-aperture" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+);
+
+export default MainTabScreen;
+
+const HomeStackScreen = ({navigation}) => (
+<HomeStack.Navigator screenOptions={{
+        headerStyle: {
+        backgroundColor: '#f1a54f',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+        fontWeight: 'bold'
+        }
+    }}>
+        <HomeStack.Screen name="Home" component={HomeScreen} options={{
+        title:'لائحة السكان',
+        headerLeft: () => (
+            <Icon.Button name="ios-menu" size={25} backgroundColor="#f1a54f" onPress={() => navigation.openDrawer()}></Icon.Button>
+        )
+        }} />
+</HomeStack.Navigator>
+);
+
+const DetailsStackScreen = ({navigation}) => (
+<DetailsStack.Navigator screenOptions={{
+        headerStyle: {
+        backgroundColor: '#0068b0',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+        fontWeight: 'bold'
+        }
+    }}>
+        <DetailsStack.Screen name="العمليات" component={DetailsScreen} options={{
+        headerLeft: () => (
+            <Icon.Button name="ios-menu" size={25} backgroundColor="#0068b0" onPress={() => navigation.openDrawer()}></Icon.Button>
+        )
+        }} />
+</DetailsStack.Navigator>
+);
+  
